fc4b777feeaef864f1e855b9450cb3baae7d1a07
Mayly12345 <58310325+Mayly12345@users.noreply.github.com>
2020-03-12 16:41:57 +0100

    Fixed empty inputs

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 document.addEventListener('DOMContentLoaded', onReady);
 
-let constIndex = 0
+let constIndex = 0;
 let withVotes = false;
 let options = document.getElementById('input');
 
 Storage.prototype.setObj = function (key, obj) {
     return this.setItem(key, JSON.stringify(obj))
-}
+};
 Storage.prototype.getObj = function (key) {
     return JSON.parse(this.getItem(key))
-}
+};
 let memory = window.localStorage.getObj('memory');
 
 
 function onReady(e) {
-    createInputRow()
+    createInputRow();
+    createInputRow();
     let votesCheckbox = document.getElementById('useVotes');
     votesCheckbox.checked = withVotes;
     votesCheckbox.addEventListener('click', toggleVotes);
     document.getElementById('choose').addEventListener('click', createAnswerAndSpitOut);
     generateInMemoryList();
-    applyVoteSetting()
+    applyVoteSetting();
     document.getElementById('addRow').addEventListener('click', createInputRow)
 
 
@@ -28,38 +29,38 @@ function onReady(e) {
 
 function createInputRowWithValue(option) {
 
-    constIndex += 1
+    constIndex += 1;
 
-    let container = document.createElement("li")
-    container.className = 'inputRowContainer'
-    container.id = constIndex
+    let container = document.createElement("li");
+    container.className = 'inputRowContainer';
+    container.id = constIndex;
 
     const optionField = document.createElement('input');
     optionField.setAttribute('type', 'text');
-    optionField.setAttribute('placeholder', 'Location or food option')
-    optionField.value = option
+    optionField.setAttribute('placeholder', 'Location or food option');
+    optionField.value = option;
 
 
     const votes = document.createElement('input');
-    votes.setAttribute('type', 'number')
-    votes.setAttribute('min', '00')
-    votes.setAttribute('value', '1')
-    votes.classList.add('voteNumber')
+    votes.setAttribute('type', 'number');
+    votes.setAttribute('min', '00');
+    votes.setAttribute('value', '1');
+    votes.classList.add('voteNumber');
     if (!withVotes) {
         votes.classList.add('hidden')
     }
 
     const removeButton = document.createElement('input');
-    removeButton.setAttribute('type', 'button')
-    removeButton.setAttribute('data-id', constIndex)
-    removeButton.setAttribute('value', 'X')
-    removeButton.addEventListener('click', removeOptionRowEventListener)
+    removeButton.setAttribute('type', 'button');
+    removeButton.setAttribute('data-id', constIndex);
+    removeButton.setAttribute('value', 'X');
+    removeButton.addEventListener('click', removeOptionRowEventListener);
 
 
-    container.appendChild(optionField)
+    container.appendChild(optionField);
 
     container.appendChild(votes);
-    container.appendChild(removeButton)
+    container.appendChild(removeButton);
 
     options.appendChild(container);
     container.addEventListener('keyup', keyHandler);
@@ -77,7 +78,7 @@ function keyHandler(e) {
 
     if (e.keyCode === 13 && parseInt(e.target.parentElement.id) == options.children[options.children.length - 1].id) {
         //  console.log(e.target.parentElement.id  == options.children[options.children.length - 1].id )
-        e.target.removeEventListener('keyup', keyHandler, true)
+        e.target.removeEventListener('keyup', keyHandler, true);
         if (!isLastRowEmpty()) {
             createInputRow()
         }
@@ -86,59 +87,57 @@ function keyHandler(e) {
 }
 
 function toggleVotes() {
-    withVotes = !withVotes
+    withVotes = !withVotes;
     applyVoteSetting()
 }
 
 function applyVoteSetting() {
-    let elements = document.getElementsByClassName('voteNumber')
+    let elements = document.getElementsByClassName('voteNumber');
     if (withVotes) {
         for (let i = 0; i < elements.length; i++) {
-            elements[i].classList.remove('hidden')
+            elements[i].classList.remove('hidden');
             elements[i].value = 1
-        };
+        }
     } else {
         for (let i = 0; i < elements.length; i++) {
-            elements[i].classList.add('hidden')
+            elements[i].classList.add('hidden');
             elements[i].value = 1
-        };
+        }
     }
 }
 
 function createAnswerAndSpitOut() {
-    if (options.children.length > 1) {
-        let weightedList = [];
-        Array.from(options.children).forEach(element => {
-            const optionText = element.children[0].value;
-            const wheight = element.children[1].value
-            if (optionText.length > 0) {
-                saveOption(optionText);
-                for (let i = 0; i < wheight; i++) {
-
-                    weightedList.push(optionText)
-
-
-                }
+    let weightedList = [];
+    let emptyOptions = false;
+    Array.from(options.children).forEach(element => {
+        const optionText = element.children[0].value;
+        const wheight = element.children[1].value;
+        if (optionText) {
+            saveOption(optionText);
+            for (let i = 0; i < wheight; i++) {
+                weightedList.push(optionText)
             }
-        });
-        //console.log(weightedList)
+        } else {
+            emptyOptions = true;
+        }
+    });
+    //console.log(weightedList)
 
-        document.getElementById('output').innerText = weightedList[Math.floor(Math.random() * weightedList.length)]
-        document.getElementById('outputSentence').classList.remove('hidden')
-
-    } else(
+    if(emptyOptions){
         document.getElementById('output').innerText = 'Please provide some options'
-    )
-
+    } else {
+        document.getElementById('output').innerText = weightedList[Math.floor(Math.random() * weightedList.length)];
+        document.getElementById('outputSentence').classList.remove('hidden');
+    }
 }
 
 function generateInMemoryList() {
     const memoryList = document.getElementById('memoryList');
-    memoryList.innerHTML = ''
+    memoryList.innerHTML = '';
     if (!memory) {
         memory = {
             options: []
-        }
+        };
         window.localStorage.setObj('memory', memory)
     }
     memory.options.forEach(option => {
@@ -149,8 +148,8 @@ function generateInMemoryList() {
 
 function saveOption(option) {
     if (!memory.options.includes(option)) {
-        console.log('Saved to memory')
-        memory.options.push(option)
+        console.log('Saved to memory');
+        memory.options.push(option);
         window.localStorage.setObj('memory', memory)
 
     }
@@ -177,21 +176,21 @@ function addMemoryItem(option) {
     const memoryText = document.createElement('span');
     memoryText.innerText = option;
 
-    const memoryAddButton = document.createElement('input')
-    memoryAddButton.setAttribute('type', 'button')
-    memoryAddButton.setAttribute('data-option', option)
-    memoryAddButton.addEventListener('click', addToOptionList)
-    memoryAddButton.value = 'use'
+    const memoryAddButton = document.createElement('input');
+    memoryAddButton.setAttribute('type', 'button');
+    memoryAddButton.setAttribute('data-option', option);
+    memoryAddButton.addEventListener('click', addToOptionList);
+    memoryAddButton.value = 'use';
 
-    const memoryRemoveButton = document.createElement('input')
-    memoryRemoveButton.setAttribute('type', 'button')
-    memoryRemoveButton.setAttribute('data-option', option)
-    memoryRemoveButton.addEventListener('click', removeFromMemoryList)
-    memoryRemoveButton.value = 'remove'
+    const memoryRemoveButton = document.createElement('input');
+    memoryRemoveButton.setAttribute('type', 'button');
+    memoryRemoveButton.setAttribute('data-option', option);
+    memoryRemoveButton.addEventListener('click', removeFromMemoryList);
+    memoryRemoveButton.value = 'remove';
 
     memoryItem.appendChild(memoryText);
     memoryItem.appendChild(memoryAddButton);
-    memoryItem.appendChild(memoryRemoveButton)
+    memoryItem.appendChild(memoryRemoveButton);
 
     memoryList.appendChild(memoryItem);
 }
@@ -201,8 +200,8 @@ function removeFromMemoryList(e) {
     const option = e.target.getAttribute('data-option');
 
     if (memory.options.includes(option)) {
-        memory.options = memory.options.filter(element => element != option)
-        window.localStorage.setObj('memory', memory)
+        memory.options = memory.options.filter(element => element != option);
+        window.localStorage.setObj('memory', memory);
         console.log('removing ' + option)
     }
     generateInMemoryList()
@@ -215,7 +214,7 @@ function removeOptionRow(id) {
 
 function removeOptionRowEventListener(e) {
 
-    removeOptionRow(e.target.getAttribute('data-id'))
+    removeOptionRow(e.target.getAttribute('data-id'));
     if (options.children.length == 0) {
         createInputRow()
     }
@@ -233,7 +232,7 @@ function isLastRowEmpty() {
 
 function optionListContains(option) {
     for (let i = 0; i < options.children.length; i++) {
-        let value = options.children[i].children[0].value
+        let value = options.children[i].children[0].value;
         //console.log(value)
         if (value == option) {
             return true
